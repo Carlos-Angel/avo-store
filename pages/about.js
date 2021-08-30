43d6984c748bb963c2ba3315ac90f911874c5a8d
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-29 20:33:07 -0500

    feat: enlazar páginas

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const about = () => {
   return (
     <div>
+      <Navbar />
       <h1>AboutPage</h1>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <h1>HomePage</h1>
     </div>
   );
