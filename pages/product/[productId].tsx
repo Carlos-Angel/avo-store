209ba69a1cb8737376384fc3ac5a8d9b86a4a678
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-30 22:37:40 -0500

    feat: obtener el producto en la página ProductItem

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const ProductItem = () => {
   const {
     query: { productId },
   } = useRouter();
+  const [product, setProduct] = useState<TProduct>();
+
+  useEffect(() => {
+    if (productId) {
+      window
+        .fetch(`/api/avo/${productId}`)
+        .then((res) => res.json())
+        .then((res) => setProduct(res))
+        .catch((err) => console.error(err.message));
+    }
+  }, [productId]);
 
   return (
     <div>
-      <h1>Product Item {productId}</h1>
+      <h1>Product {product?.name}</h1>
+      <p>$ {product?.price}</p>
     </div>
   );
 };
