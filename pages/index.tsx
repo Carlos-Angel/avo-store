5aa1b69ed648facf667803a07b96c54ebd873cc1
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-30 22:36:55 -0500

    feat: obtener los products en la página Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
+  const [productList, setProductList] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((res) => res.json())
+      .then(({ data }) => setProductList(data))
+      .catch(() => setProductList([]));
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <h1>HomePage</h1>
+      <h1>Avocados</h1>
+      {productList.map((product) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
     </div>
   );
 };
