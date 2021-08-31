3144f180587bc550e069af3d750c54f914129b07
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-30 23:27:11 -0500

    feat: implementando Layout

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Navbar from '../../components/Navbar';
 
 const about = () => {
   return (
     <div>
-      <Navbar />
       <h1>AboutPage</h1>
     </div>
   );
