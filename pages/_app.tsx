bd9836096a73bd570d6f03f4ca9adec91ebb22ae
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-31 09:58:23 -0500

    feat: cargar estilos y el CartProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app';
-import Layout from '@components/Layout';
+import 'semantic-ui-css/semantic.min.css';
+import '../global.css';
+
+import CartProvider from '@store/Cart';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
   );
 }
 
